eaddd1757b3738b4d121774b95b3a8c5e6bede2e
BIVILLE Allan <biviall@ndlp.fr>
2021-09-09 11:53:38 +0200

    Second commit

--- Personnage.php
+++ Personnage.php
@@ -1,16 +1,50 @@
 <?php
 // Présence du mot clé class + nom de la classe
-class Personnage {
+class Personnage
+{
 // Declaration des attributs et méthodes ici.
+    private $_nom = 'Inconnu';
+    private $_force = 50;
+    private $_experience = 1;
+    private $_degats = 0;
+
+    public function definirForce($force){
+        $this->_force = $force;
+    }
+
+    public function definirDegats($degats){
+        $this->_degats = $degats;
+    }
+    public function afficherDegats()
+    {
+        return $this->_degats;
+    }
+    public function definirExperience($experience){
+        $this->_experience = $experience;
+    }
+
+    public function parler()
+    {
+        print("Je suis un personnage");
+    }
+
+
+    public function frapper($adversaire)
+    {
+        // $adversaire->_degats = $adversaire->_degats + $this->_force;
+        $adversaire->_degats += $this->_force;
+        $this->gagnerExperience();
+    }
+
+    public function gagnerExperience()
+    {
+        $this->_experience++;
+    }
+
+    public function afficherExperience()
+    {
+        return $this->_experience;
+    }
+
 
 }
-
-
-
-
-
-
-
-
-
-?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include 'Personnage.php';
+
+print ("<h1><center>Jeu de combat</center></h1>");
+
+// $perso1->Parler();
+// $perso1->gagnerExperience();
+// print("Expérience = " . $perso1->afficherExperience());
+
+
+$perso1 = new Personnage();
+$perso1->definirForce(20);
+$perso1->definirExperience(15);
+
+$perso2 = new Personnage();
+$perso2->definirForce(60);
+$perso2->definirExperience(1);
+
+$perso1->frapper($perso2);
+$perso2->frapper($perso1);
+
+print("<br/>Dégats du joueur 1 = " . $perso1->afficherDegats());
+print("<br/>Dégats du joueur 2 = " . $perso2->afficherDegats());
+
+
+
