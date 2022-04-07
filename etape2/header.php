5e6e11ff1cd28245e7c8c9e5b8034388c8c2da3d
BIVILLE Allan <biviall@ndlp.fr>
2022-04-07 16:31:57 +0200

    commit

--- etape2/header.php
+++ etape2/header.php
@@ -1,10 +1,11 @@
 <?php
+spl_autoload_register('chargerClasse');
 function chargerClasse(string $classe)
 {
     include $classe . '.php';
     // include_once $classe . '.php';
     // require $classe . '.php';
 }
-spl_autoload_register('chargerClasse');
+
 
 include "conf.php";
